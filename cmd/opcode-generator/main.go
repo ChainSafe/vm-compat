05eb4d8e1607043ddf9684c3ce6f7bed5df4b4f2
ohm <ohm.patel1997@gmail.com>
2025-01-17 17:17:50 +0400

    use the first six bits

--- cmd/opcode-generator/main.go
+++ cmd/opcode-generator/main.go
@@ -12,67 +12,6 @@ import (
 	"strings"
 )
 
-type InstructionSet string
-
-const (
-	R_INSTRUCTION  InstructionSet = "R"
-	I_INSTRUCTION  InstructionSet = "I"
-	J_INSTRUCTION  InstructionSet = "J"
-	FR_INSTRUCTION InstructionSet = "FR"
-	FI_INSTRUCTION InstructionSet = "FI"
-)
-
-var (
-	// ref: https://en.wikibooks.org/wiki/MIPS_Assembly/Instruction_Formats#FI_Instructions
-	memnomicToInstructionSet = map[string]InstructionSet{
-		"add":   R_INSTRUCTION,
-		"addi":  I_INSTRUCTION,
-		"addiu": I_INSTRUCTION,
-		"addu":  R_INSTRUCTION,
-		"and":   R_INSTRUCTION,
-		"andi":  I_INSTRUCTION,
-		"beq":   I_INSTRUCTION,
-		"blez":  I_INSTRUCTION,
-		"bne":   I_INSTRUCTION,
-		"bgtz":  I_INSTRUCTION,
-		"div":   R_INSTRUCTION,
-		"divu":  R_INSTRUCTION,
-		"j":     J_INSTRUCTION,
-		"jal":   J_INSTRUCTION,
-		"jalr":  R_INSTRUCTION,
-		"jr":    R_INSTRUCTION,
-		"lb":    I_INSTRUCTION,
-		"lbu":   I_INSTRUCTION,
-		"lhu":   I_INSTRUCTION,
-		"lui":   I_INSTRUCTION,
-		"lw":    I_INSTRUCTION,
-		"mfhi":  R_INSTRUCTION,
-		"mthi":  R_INSTRUCTION,
-		"mflo":  R_INSTRUCTION,
-		"mtlo":  R_INSTRUCTION,
-		"mfc0":  R_INSTRUCTION,
-		"mult":  R_INSTRUCTION,
-		"multu": R_INSTRUCTION,
-		"nor":   R_INSTRUCTION,
-		"xor":   R_INSTRUCTION,
-		"or":    R_INSTRUCTION,
-		"ori":   I_INSTRUCTION,
-		"sb":    I_INSTRUCTION,
-		"sh":    I_INSTRUCTION,
-		"slt":   R_INSTRUCTION,
-		"slti":  I_INSTRUCTION,
-		"sltiu": I_INSTRUCTION,
-		"sltu":  R_INSTRUCTION,
-		"sll":   R_INSTRUCTION,
-		"srl":   R_INSTRUCTION,
-		"sra":   R_INSTRUCTION,
-		"sub":   R_INSTRUCTION,
-		"subu":  R_INSTRUCTION,
-		"sw":    I_INSTRUCTION,
-		"bnez":  I_INSTRUCTION,
-	}
-)
-
 type Instruction struct {
 	Address   string
 	Opcode    string
@@ -195,11 +134,7 @@ func parseAsmOutput(input string) ([]Instruction, error) {
 		line = strings.TrimSpace(line)
 
 		if matches := asmLineRe.FindStringSubmatch(line); len(matches) > 0 {
-			iset, ok := memnomicToInstructionSet[matches[4]]
-			if !ok {
-				continue
-			}
-			hexNumber, err := parseOpcodeHex(matches[3], iset)
+			hexNumber, err := parseOpcodeHex(matches[3])
 			if err != nil {
 				fmt.Println("line: ", line)
 				fmt.Println("matches: ", matches)
@@ -253,21 +188,18 @@ func parseArgs(argsStr string) []string {
 	return args
 }
 
-func parseOpcodeHex(str string, instructionSet InstructionSet) (string, error) {
+func parseOpcodeHex(str string) (string, error) {
 	// parse the hex string to uint64
 	i, err := strconv.ParseUint(str, 16, 32)
 	if err != nil {
 		return "", err
 	}
 
-	switch instructionSet {
-	case R_INSTRUCTION, FR_INSTRUCTION:
-		// R-type instructions have last 6 bits for the opcode, funct part
-		return fmt.Sprintf("0x%x", i&0x3F), nil
-	case I_INSTRUCTION, J_INSTRUCTION, FI_INSTRUCTION:
-		// I-type instructions have first 6 bits for the opcode
-		return fmt.Sprintf("0x%x", i>>0x1A), nil
-	default:
-		return "", fmt.Errorf("unknown instruction set: %s", instructionSet)
+	fixSixBits := i >> 0x1A
+
+	if fixSixBits == 0 { // R Instruction
+		return fmt.Sprintf("0x%x", i&0x3F), nil // return last 6 bits
 	}
+
+	return fmt.Sprintf("0x%x", i>>0x1A), nil // return first 6 bits
 }
