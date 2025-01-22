25ba88eb2bab3d43a5121028d5d19a4b24f80bbb
ohm <ohm.patel1997@gmail.com>
2025-01-22 22:59:26 +0400

    added opcode and diassembler modules (#14)
    * added opcode and diassembler module
    
    * added disassembler behind the interface
    
    * refactor opcode
    
    * refactor
    
    * added the opcode analyzer to main
    
    * fixed comments
    
    * fix options
    
    * updated the flag names
    
    * added disambler manager
    
    * fixed bugs
    
    * Added error check
    
    ---------
    
    Co-authored-by: Arun Dhyani <dhyaniarun7@gmail.com>

--- cmd/analyser/main.go
+++ cmd/analyser/main.go
@@ -6,13 +6,20 @@ import (
 	"html/template"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/ChainSafe/vm-compat/analysis"
+	"github.com/ChainSafe/vm-compat/disassembler"
+	"github.com/ChainSafe/vm-compat/disassembler/manager"
+	"github.com/ChainSafe/vm-compat/opcode"
 	"github.com/ChainSafe/vm-compat/profile"
 )
 
 var (
-	vmProfile = flag.String("vm-profile", "", "vm profile config")
+	vmProfile             = flag.String("vm-profile", "", "vm profile config")
+	analyzer              = flag.String("analyzer", "opcode", "analyzer to run. Options: opcode, syscall")
+	mode                  = flag.String("mode", "binary", "mode to run. only required for mode `opcode`. Options: binary, source")
+	disassemblyOutputPath = flag.String("disassembly-output-path", "", "output file path for opcode assembly code. optional. only required for mode `opcode`. only specify if you want to write assembly code to a file")
 )
 
 const usage = `
@@ -20,7 +27,7 @@ analyser: checks the program compatibility against the vm profile
 
 Usage:
 
-  callgraph [-vm-profile=path_to_config] package...
+  callgraph [-analyzer=opcode|syscall] [-vm-profile=path_to_config] package...
 `
 
 type WebData struct {
@@ -42,8 +49,50 @@ func main() {
 		log.Fatalf("Error loading profile: %v", err)
 	}
 
-	err = analysis.AnalyseSyscalls(profile, args...)
-	if err != nil {
-		panic(err)
+	switch *analyzer {
+	case "opcode":
+		err = analyzeOpcode(profile, args...)
+		if err != nil {
+			panic(err)
+		}
+	case "syscall":
+		err = analysis.AnalyseSyscalls(profile, args...)
+		if err != nil {
+			panic(err)
+		}
+	default:
+		log.Fatalf("Invalid analyzer: %s", *analyzer)
 	}
+}
+
+func analyzeOpcode(profile *profile.VMProfile, paths ...string) error {
+	if len(paths) == 0 {
+		return fmt.Errorf("no paths provided for opcode analysis")
+	}
+
+	dis, err := manager.NewDisassembler(disassembler.TypeObjdump, profile.GOOS, profile.GoArch)
+	if err != nil {
+		return err
+	}
+
+	if *disassemblyOutputPath == "" {
+		// add a temporary path to write the disassembly output
+		*disassemblyOutputPath = filepath.Join(os.TempDir(), "temp_assembly_ouput")
+		defer os.Remove(*disassemblyOutputPath)
+	}
+
+	switch *mode {
+	case "binary":
+		_, err = dis.Disassemble(disassembler.SourceBinary, paths[0], *disassemblyOutputPath)
+		if err != nil {
+			return err
+		}
+	case "source":
+		_, err = dis.Disassemble(disassembler.SourceFile, paths[0], *disassemblyOutputPath)
+		if err != nil {
+			return err
+		}
+	}
+
+	return opcode.AnalyseOpcodes(profile, *disassemblyOutputPath)
 }
