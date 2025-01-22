a9256b5d6dc583ec7dc4a6aa99978bdd77017e98
ohm <ohm.patel1997@gmail.com>
2025-01-22 17:25:24 +0400

    fix options

--- cmd/analyser/main.go
+++ cmd/analyser/main.go
@@ -17,7 +17,7 @@ import (
 var (
 	vmProfile             = flag.String("vm-profile", "", "vm profile config")
 	analyzer              = flag.String("analyzer", "opcode", "analyzer to run. Options: opcode, syscall")
-	mode                  = flag.String("mode", "binary", "mode to run. Options: mips")
+	mode                  = flag.String("mode", "binary", "mode to run. Options: binary, source")
 	disassemblyOutputPath = flag.String("disassembly-output-path", "", "output file path for opcode assembly code. optional. only specify if you want to write assembly code to a file")
 )
 
