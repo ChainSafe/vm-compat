80d909862c43c89b37d9a781f30492cda327220b
ohm <ohm.patel1997@gmail.com>
2025-03-10 16:32:55 +0400

    Report comparer (#49)
    * added the report comparer
    
    * removed double append
    
    * replaced md5 with sha256
    
    * lint fixes
    
    * changed addressed
    
    * removed slice
    
    * lint fixes
    
    * lint fixes
    
    * reuse code
    
    * cehck for existence
    
    * refactor code

--- renderer/json.go
+++ renderer/json.go
@@ -16,7 +16,7 @@ func NewJSONRenderer() Renderer {
 }
 
 func (r *JSONRenderer) Render(issues []*analyzer.Issue, output io.Writer) error {
-	return json.NewEncoder(output).Encode(issues)
+	return json.NewEncoder(output).Encode(analyzer.SortIssues(issues))
 }
 
 func (r *JSONRenderer) Format() string {
